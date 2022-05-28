f298ed3b031ea96a4531ae39e018453e8ff62988
minssu86 <manager.kim86@gmail.com>
2022-05-28 09:17:30 +0900

    5월 28일 강의 시작 커밋

--- src/main/java/com/sparta/springcore/service/ProductService.java
+++ src/main/java/com/sparta/springcore/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.sparta.springcore.service;
 
-import com.sparta.springcore.model.Product;
 import com.sparta.springcore.dto.ProductMypriceRequestDto;
-import com.sparta.springcore.repository.ProductRepository;
 import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -12,23 +14,23 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository){
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
+// 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto, userId);
 
-    public Product createProduct(ProductRequestDto requestDto) {
-        Product product =new Product(requestDto);
         productRepository.save(product);
 
         return product;
     }
 
-
     public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
-
-        Product product = productRepository.findById(id).orElseThrow(
-                ()->new NullPointerException("해당 아이디가 존재하지 않습니다."));
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NullPointerException("해당 아이디가 존재하지 않습니다."));
 
         int myprice = requestDto.getMyprice();
         product.setMyprice(myprice);
@@ -37,9 +39,13 @@ public class ProductService {
         return product;
     }
 
+    // 회원 ID 로 등록된 상품 조회
+    public List<Product> getProducts(Long userId) {
+        return productRepository.findAllByUserId(userId);
+    }
 
-    public List<Product> getProducts() {
-        List<Product> products = productRepository.findAll();
-        return products;
+    // (관리자용) 상품 전체 조회
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
